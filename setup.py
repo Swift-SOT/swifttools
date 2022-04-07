3d104ef250992f25bcdb30d41f157f1f823c8286
Jamie Kennea <jak51@psu.edu>
2022-04-07 15:24:56 -0400

    Migrate from setup.py to setup.cfg / pyproject.toml

--- setup.py
+++ setup.py
@@ -1,28 +1,4 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-PKGNAME = "swifttools"
-
-setuptools.setup(
-    name=PKGNAME,
-    version="2.4.2",
-    author="Phil Evans",
-    author_email="pae9@leicester.ac.uk",
-    description="Tools for users of the Swift satellite",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://gitlab.com/DrPhilEvans/swifttools",
-    packages=[PKGNAME] + [f'{PKGNAME}.{p}' for p in setuptools.find_packages(where=PKGNAME)],
-    install_requires=['requests', 'python-jose', 'pandas', 'tabulate', 'numpy'],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
-
-
-
+if __name__ == "__main__":
+    setuptools.setup()
